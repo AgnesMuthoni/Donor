39a87afa2ae8f0c1f0335f095af73eaa9eb21112
AgnesMuthoni <karonjiagnesm@gmail.com>
2014-09-15 16:24:19 +0300

    Project First Commit

---
+++ src/main/java/org/karonji/donor/Donor.java
@@ -0,0 +1,13 @@
+package org.karonji.donor;
+
+/**
+ * Hello world!
+ *
+ */
+public class Donor
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
